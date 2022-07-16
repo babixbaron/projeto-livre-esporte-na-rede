2a11c8ff007fdcf7f14aef44f7b153c7fa546b09
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 19:47:28 -0300

    :sparkles: adiciona modelo para a coleção de projetos sociais

---
+++ src/models/projetosModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const projetosSchema = mongoose.Schema(
+    {
+        _id: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId },
+        nome: { type: String, required: true},
+        contato: { type: Number, required: true },
+        endereço: { type: String, required: true },
+        regiao: { type: String },
+        vagasDisponiveis: { type: Boolean },
+        diasDaSemana: [{ type: String}],
+        modalidades: [{ type: String}],
+    },
+    {
+        versionKey: false,
+    }
+)
+
+const projetos = mongoose.model('projetos', projetosSchema)
+
+module.exports = projetos
