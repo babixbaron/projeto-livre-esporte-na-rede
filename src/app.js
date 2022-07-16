d7d815a8148f699b73c036988975279d80262bf1
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 19:20:06 -0300

    :sparkles: adiciona funcionalidades de documentação no app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ require('dotenv-safe').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
 
 const app = express()
 
@@ -10,6 +12,7 @@ mongoose.connect()
 
 app.use(cors())
 app.use(express.json())
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 
 module.exports = app
