676c07a970d57c2ffd8089930d3bac662393047c
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 20:35:05 -0300

    :sparkles: adiciona funcionalidade de rotas

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 require('dotenv-safe').config()
-
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('../swagger/swagger_output.json')
-
+const index = require('./routes/index')
 const app = express()
+const projetosRoutes = require('./routes/projetosRoutes')
 
 mongoose.connect()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/', index)
+app.use(projetosRoutes)
 app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 
