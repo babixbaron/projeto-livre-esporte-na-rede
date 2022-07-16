c84cf44cc5062dd0b992548c8a73db8d8f369298
Bárbara Baron <bcostabaron@gmail.com>
2022-07-16 18:49:34 -0300

    :wrench: configura conexão da base de dados

---
+++ src/database/mongooseConnect.js
@@ -0,0 +1,21 @@
+const MONGO_URI = process.env.MONGO_URI
+
+const mongoose = require('mongoose')
+
+const connect = async() => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log('Banco conectado.')
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {
+    connect
+}
