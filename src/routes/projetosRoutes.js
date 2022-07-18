26d91d6e2547b2569b25955e1b909db11f42158d
Bárbara Baron <bcostabaron@gmail.com>
2022-07-18 12:42:42 -0300

    :sparkles: adiciona rota para encontrar todos os projetos

--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('../controller/projetosController')
 
 router.post('/projeto', controller.registerNewProject)
+router.get('/projetos', controller.findAllProjects)
 
 module.exports = router
