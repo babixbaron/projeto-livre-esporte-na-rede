e5318316ba5b2b92a5ccf0b0c08e55935c9526f2
Bárbara Baron <bcostabaron@gmail.com>
2022-07-18 15:28:54 -0300

    :memo: altera nome de uma rota

--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -4,7 +4,7 @@ const controller = require('../controller/projetosController')
 
 router.post('/projeto', controller.registerNewProject)
 router.get('/projetos', controller.findAllProjects)
-router.get('/projetos/:id', controller.findProjectById)
+router.get('/projeto/:id', controller.findProjectById)
 router.get('/projeto/regiao', controller.findProjectByRegiao)
 router.get('/projeto/modalidade', controller.findProjectByModalidade)
 router.patch('/projeto/atualizar/:id', controller.updateProject)
