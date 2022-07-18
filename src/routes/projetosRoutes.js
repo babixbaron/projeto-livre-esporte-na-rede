90a4500518577848a909ba3d45a300eac33efd48
Bárbara Baron <bcostabaron@gmail.com>
2022-07-18 13:00:13 -0300

    :sparkles: adiciona rota para atualizar parte do projeto

--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -5,5 +5,6 @@ const controller = require('../controller/projetosController')
 router.post('/projeto', controller.registerNewProject)
 router.get('/projetos', controller.findAllProjects)
 router.get('/projetos/:id', controller.findProjectById)
+router.patch('/projeto/atualizar/:id', controller.updateProject)
 
 module.exports = router
