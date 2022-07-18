2cf54700cb4606687de457683ec08658476ebee1
Bárbara Baron <bcostabaron@gmail.com>
2022-07-18 13:06:02 -0300

    :sparkles: adiciona rota para remover projeto

--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -6,5 +6,6 @@ router.post('/projeto', controller.registerNewProject)
 router.get('/projetos', controller.findAllProjects)
 router.get('/projetos/:id', controller.findProjectById)
 router.patch('/projeto/atualizar/:id', controller.updateProject)
+router.delete('/projeto/remover/:id', controller.deleteProject)
 
 module.exports = router
