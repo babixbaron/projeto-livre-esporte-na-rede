2ea06d0d4c2924cc60115df112243bb3414d07c3
Bárbara Baron <bcostabaron@gmail.com>
2022-07-19 16:06:24 -0300

    :zap: muda a ordem das rotas para otimizar o acesso

--- src/routes/projetosRoutes.js
+++ src/routes/projetosRoutes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/projetosController')
 
-router.post('/projeto', controller.registerNewProject)
 router.get('/projetos', controller.findAllProjects)
 router.get('/projeto/:id', controller.findProjectById)
 router.get('/projeto/regiao', controller.findProjectByRegiao)
 router.get('/projeto/modalidade', controller.findProjectByModalidade)
+router.post('/projeto', controller.registerNewProject)
 router.patch('/projeto/atualizar/:id', controller.updateProject)
 router.delete('/projeto/remover/:id', controller.deleteProject)
 
