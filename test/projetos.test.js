ab47c4508e9403cbd51e6cdb43733eb8c78f777f
Bárbara Baron <bcostabaron@gmail.com>
2022-07-23 14:50:29 -0300

    :white_check_mark: adiciona teste de model

---
+++ test/projetos.test.js
@@ -0,0 +1,47 @@
+const Projeto = require('../src/models/projetosModel')
+
+describe('Teste de model', () => {
+  const projetos = new Projeto({
+    id: '1',
+    nome: 'Barbara Sports',
+    contato: 48996133331,
+    local: 'Florianópolis',
+    regiao: 'Centro',
+    vagasDisponiveis: true,
+    diasDaSemana: ['segunda', 'quarta'],
+    modalidades: ['voleibol'],
+    idades: '11 a 14 anos'
+  })
+
+  it('Testar nome', () => {
+    expect(projetos.nome).toBe('Barbara Sports')
+  })
+  it('Testar contato', () => {
+    expect(projetos.contato).toStrictEqual(48996133331)
+  })
+  it('Testar local', () => {
+    expect(projetos.local).toBe('Florianópolis')
+  })
+  it('Testar região', () => {
+    expect(projetos.regiao).toBe('Centro')
+  })
+  it('Testar vagas', () => {
+    expect(projetos.vagasDisponiveis).toBe(true)
+  })
+  it('Testar dias da semana', () => {
+    expect(projetos.diasDaSemana).toStrictEqual(['segunda', 'quarta'])
+  })
+  it('Testar modalidades', () => {
+    expect(projetos.modalidades).toStrictEqual(['voleibol'])
+  })
+  it('Testar idade', () => {
+    expect(projetos.idades).toBe('11 a 14 anos')
+  })
+  it('Novo projeto no banco de dados', () => {
+    projetos.save().then((dados) => {
+      expect(dados.title).toBe(' ')
+    })
+  })
+})
+
+
